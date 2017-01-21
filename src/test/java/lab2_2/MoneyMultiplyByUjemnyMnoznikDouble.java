85a603a2b6cbd4c3c2c506e0ab283a394eb34cca
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:20:11 +0100

    Update testu MoneyMultiplyByUjemnyMnoznikDouble

--- src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikDouble.java
+++ src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikDouble.java
@@ -5,11 +5,15 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class MoneyMultiplyByUjemnyMnoznikDouble {
 	
 	@Test
 	public void test(){
 		
+		Money money1=new Money(4.0,"USD");
+		double mnoznik=-2;
 		
 	}
 
