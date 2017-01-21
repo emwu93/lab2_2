47fea692194f66a17e36b718a806278f8b97fa82
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:59:30 +0100

    Update testu MoneyLessThanNegatywnyWynik

--- src/test/java/lab2_2/MoneyLessThanNegatywnyWynik.java
+++ src/test/java/lab2_2/MoneyLessThanNegatywnyWynik.java
@@ -1,6 +1,22 @@
 package lab2_2;
 
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class MoneyLessThanNegatywnyWynik {
 
+	@Test
+	public void test(){
+		
+		Money money1=new Money(3.0,"USD");
+		Money money2=new Money(2.0,"USD");
+		boolean tmp=false;
+		
+		
+		
+		
+		
+	}
 	
 }
