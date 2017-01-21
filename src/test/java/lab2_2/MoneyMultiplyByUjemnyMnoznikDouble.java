3f97adde609e5ca54b00dc2e95c3f4d284847cad
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:18:43 +0100

    Dodanie testu MoneyMultiplyByUjemnyMnoznikDouble

---
+++ src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikDouble.java
@@ -0,0 +1,16 @@
+package lab2_2;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class MoneyMultiplyByUjemnyMnoznikDouble {
+	
+	@Test
+	public void test(){
+		
+		
+	}
+
+}
