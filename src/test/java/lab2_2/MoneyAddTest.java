e3e7494f51d60c6f1b566b7d25c6dc876bb355e7
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 06:15:52 +0100

    Dodanie testu metody Add z klasy Money

---
+++ src/test/java/lab2_2/MoneyAddTest.java
@@ -0,0 +1,22 @@
+package lab2_2;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+
+public class MoneyAddTest {
+
+	@Test
+	public void test(){
+		
+		
+		Money money1=new Money(3.0);
+		Money money2=new Money(1.0);
+		
+	}
+	
+}
