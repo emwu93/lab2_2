82f564c0c8c2245c80e9ae038e2e9111c965e302
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 17:08:20 +0100

    Update testu MoneyLessThanNegatywnyWynik oraz dodanie testu MoneyLessOrEqualsRowneWartosci

---
+++ src/test/java/lab2_2/MoneyLessOrEqualsRowneWartosci.java
@@ -0,0 +1,10 @@
+package lab2_2;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+
+public class MoneyLessOrEqualsRowneWartosci {
+
+	
+	
+}
--- src/test/java/lab2_2/MoneyLessThanNegatywnyWynik.java
+++ src/test/java/lab2_2/MoneyLessThanNegatywnyWynik.java
@@ -3,6 +3,8 @@ package lab2_2;
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 public class MoneyLessThanNegatywnyWynik {
 
@@ -13,10 +15,7 @@ public class MoneyLessThanNegatywnyWynik {
 		Money money2=new Money(2.0,"USD");
 		boolean tmp=false;
 		
-		
-		
-		
-		
+		assertThat(tmp,is(equalTo(money1.lessThan(money2))));		
 	}
 	
 }
