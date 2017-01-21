a689a941c67e20ecbd8c7901a6754e647fe1179a
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:46:33 +0100

    Update testu MoneySubstractRoznaWaluta

--- src/test/java/lab2_2/MoneySubstractRoznaWaluta.java
+++ src/test/java/lab2_2/MoneySubstractRoznaWaluta.java
@@ -1,5 +1,18 @@
 package lab2_2;
 
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+
 public class MoneySubstractRoznaWaluta {
 
+	Money money1=new Money(5.0,"USD");
+	Money money2=new Money(3.0,"USD");
+	
+	
+	
 }
