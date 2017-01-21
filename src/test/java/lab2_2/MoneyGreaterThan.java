c3e481c7a2148d5f6b1bd24119a3e25e720e0053
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:32:10 +0100

    Dodanie testu MoneyGreaterThan

---
+++ src/test/java/lab2_2/MoneyGreaterThan.java
@@ -0,0 +1,18 @@
+package lab2_2;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MoneyGreaterThan {
+
+	@Test
+	public void test(){
+		
+		
+		
+	}
+	
+	
+}
--- src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikBigDecimal.java
+++ src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikBigDecimal.java
@@ -2,6 +2,8 @@ package lab2_2;
 
 import org.junit.Test;
 import java.math.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyMultiplyByUjemnyMnoznikBigDecimal {
@@ -12,6 +14,8 @@ public class MoneyMultiplyByUjemnyMnoznikBigDecimal {
 		Money money1=new Money(4.0,"USD");
 		BigDecimal mnoznik=new BigDecimal(-2.0);
 		Money oczekiwana_wart=new Money(-8.0, "USD");
+		
+		assertThat(oczekiwana_wart,is(equalTo(money1.multiplyBy(mnoznik))));
 		
 		
 	}
