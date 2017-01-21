cf59c5dca4d5e04d2e7edd110cad249ef1403c32
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:33:55 +0100

    Zmiana testu

--- src/test/java/lab2_2/MoneyAddDodwanieTakaSamaWaluta.java
+++ src/test/java/lab2_2/MoneyAddDodwanieTakaSamaWaluta.java
@@ -8,9 +8,19 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 
-public class MoneyCompatibileCurrencyTakieSameWaluty {
+public class MoneyAddDodwanieTakaSamaWaluta {
 	
+	@Test
+	public void test(){
 	Money money1=new Money(2.0,"USD");
 	Money money2=new Money(3.0,"USD");
+	Money oczekiwana_wart= new Money(5.0,"USD");
+	
+	Money suma=money1.add(money2);	
+	
+	assertThat(suma,is(equalTo(oczekiwana_wart)));
+	
+	
+	}
 
 }
