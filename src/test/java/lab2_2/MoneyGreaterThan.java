789ac7a42b3691dbd991b1c41f275b5a445128ae
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:39:38 +0100

    Update testu MoneyGreaterThan

--- src/test/java/lab2_2/MoneyGreaterThan.java
+++ src/test/java/lab2_2/MoneyGreaterThan.java
@@ -2,6 +2,8 @@ package lab2_2;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -10,7 +12,11 @@ public class MoneyGreaterThan {
 	@Test
 	public void test(){
 		
+		Money money1=new Money(2.0,"USD");
+		Money money2=new Money(1.0,"USD");
+		boolean tmp=true;
 		
+		assertThat(tmp,is(equalTo(money1.greaterThan(money2))));
 		
 	}
 	
