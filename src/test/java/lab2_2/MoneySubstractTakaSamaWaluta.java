d03028cef72905c3d047eabb8bec6e8d2d8b5a75
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:41:27 +0100

    Update testu MoneySubstractTakaSamaWaluta

--- src/test/java/lab2_2/MoneySubstractTakaSamaWaluta.java
+++ src/test/java/lab2_2/MoneySubstractTakaSamaWaluta.java
@@ -1,5 +1,16 @@
 package lab2_2;
 
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+
 public class MoneySubstractTakaSamaWaluta {
+	
+	Money money1=new Money(5.0,"USD");
+	Money money2=new Money(2.0, "USD");
 
 }
