6221eba115c671b86d7061ccf4fc4935b749e373
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:55:59 +0100

    update testu MoneySubstractTakaSamaWalutaWynikUjemny

--- src/test/java/lab2_2/MoneySubstractTakaSamaWalutaWynikUjemny.java
+++ src/test/java/lab2_2/MoneySubstractTakaSamaWalutaWynikUjemny.java
@@ -2,12 +2,15 @@ package lab2_2;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class MoneySubstractTakaSamaWalutaWynikUjemny {
 	
 	@Test
 	public void test(){
 		
-		
+		Money money1=new Money(2.0,"USD");
+		Money money2=new Money(3.0,"USD");
 		
 	}
 
