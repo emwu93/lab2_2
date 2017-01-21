a8f46b8d6157114084885e38c4bd0e10a7514da6
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 17:14:10 +0100

    Update testu MoneyLessOrEqualsRowneWartosci

--- src/test/java/lab2_2/MoneyLessOrEqualsRowneWartosci.java
+++ src/test/java/lab2_2/MoneyLessOrEqualsRowneWartosci.java
@@ -2,9 +2,19 @@ package lab2_2;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 
 public class MoneyLessOrEqualsRowneWartosci {
-
 	
+	@Test
+	public void test(){
+	Money money1=new Money(2.0,"USD");
+	Money money2=new Money(2.0,"USD");
+	boolean tmp=true;
 	
+	assertThat(tmp,is(equalTo(money1.lessOrEquals(money2))));
+	}
 }
