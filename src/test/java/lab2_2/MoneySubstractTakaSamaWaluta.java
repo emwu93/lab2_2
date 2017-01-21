bb0293d18a3757707382a68db47e2b8edf69f40f
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:43:11 +0100

    Update testu MoneySubstractTakaSamaWaluta v2

--- src/test/java/lab2_2/MoneySubstractTakaSamaWaluta.java
+++ src/test/java/lab2_2/MoneySubstractTakaSamaWaluta.java
@@ -10,7 +10,12 @@ import static org.junit.Assert.assertThat;
 
 public class MoneySubstractTakaSamaWaluta {
 	
+	@Test
+	public void test(){
 	Money money1=new Money(5.0,"USD");
 	Money money2=new Money(2.0, "USD");
-
+	Money oczekiwana_wart=new Money(3.0,"USD");
+	
+	assertThat(oczekiwana_wart,is(equalTo(money1.)))
+	}
 }
