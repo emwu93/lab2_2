7f09d421894dcfd6cbc8d9f56985ca481084b1e6
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:49:16 +0100

    Update testu MoneySubstractRoznaWaluta v4

--- src/test/java/lab2_2/MoneySubstractRoznaWaluta.java
+++ src/test/java/lab2_2/MoneySubstractRoznaWaluta.java
@@ -4,19 +4,15 @@ import org.junit.Test;
 
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
-
 
 public class MoneySubstractRoznaWaluta {
 
 	@Test(expected=IllegalArgumentException.class)
 	public void test(){
 	Money money1=new Money(5.0,"USD");
-	Money money2=new Money(3.0,"USD");
+	Money money2=new Money(3.0,"EUR");
 	
-	
-	
+	money1.subtract(money2);	
 	
 	}
 	
