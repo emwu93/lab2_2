f4d5570dc616d163582a2cbf322933658b550138
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:07:28 +0100

    Update testu Money MultiplyByDodadniMnoznik v2

--- src/test/java/lab2_2/MoneyMultiplyByDodadniMnoznik.java
+++ src/test/java/lab2_2/MoneyMultiplyByDodadniMnoznik.java
@@ -3,6 +3,9 @@ package lab2_2;
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import java.math.*;
 
 public class MoneyMultiplyByDodadniMnoznik {
 	
@@ -10,6 +13,9 @@ public class MoneyMultiplyByDodadniMnoznik {
 	public void test(){
 		
 		Money money1=new Money(2.0,"USD");
+		BigDecimal mnoznik= new BigDecimal(2.0);
+		
+		
 		
 	}
 
