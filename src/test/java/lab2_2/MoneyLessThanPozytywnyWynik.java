9e35e50649e0a8131d356681e2dbe8c3a4f76959
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:52:33 +0100

    Update testu MoneyLessThanPozytywnyWynik

--- src/test/java/lab2_2/MoneyLessThanPozytywnyWynik.java
+++ src/test/java/lab2_2/MoneyLessThanPozytywnyWynik.java
@@ -2,12 +2,18 @@ package lab2_2;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 public class MoneyLessThanPozytywnyWynik {
 	
 	@Test
 	public void test(){
 		
-		
+	Money money1=new Money(2.0,"USD");
+	Money money2=new Money(3.0,"USD");
 		
 		
 		
