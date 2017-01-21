1cfa65d6b8d7a684c2753c04bfaef23fe87ea525
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:27:24 +0100

    Update testu MoneyMultiplyByUjemnyMnoznikBigDecimal v2

--- src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikBigDecimal.java
+++ src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikBigDecimal.java
@@ -10,6 +10,8 @@ public class MoneyMultiplyByUjemnyMnoznikBigDecimal {
 	public void test(){
 		
 		Money money1=new Money(4.0,"USD");
+		BigDecimal mnoznik=new BigDecimal(-2.0);
+		Money oczekiwana_wart=new Money(-8.0, "USD");
 		
 		
 	}
