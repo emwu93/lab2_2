8115e8ddd5998b0572d634be4570032cb32b3f99
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:04:38 +0100

    Update testu Money MultiplyByDodadniMnoznik

--- src/test/java/lab2_2/MoneyMultiplyByDodadniMnoznik.java
+++ src/test/java/lab2_2/MoneyMultiplyByDodadniMnoznik.java
@@ -1,5 +1,16 @@
 package lab2_2;
 
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public class MoneyMultiplyByDodadniMnoznik {
+	
+	@Test
+	public void test(){
+		
+		Money money1=new Money(2.0,"USD");
+		
+	}
 
 }
