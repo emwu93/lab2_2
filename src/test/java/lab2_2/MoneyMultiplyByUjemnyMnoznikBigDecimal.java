6eb6269ccdb7c2797c23bed425491a50b989999d
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:25:53 +0100

    Update testu MoneyMultiplyByUjemnyMnoznikBigDecimal

--- src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikBigDecimal.java
+++ src/test/java/lab2_2/MoneyMultiplyByUjemnyMnoznikBigDecimal.java
@@ -1,12 +1,15 @@
 package lab2_2;
 
 import org.junit.Test;
+import java.math.*;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyMultiplyByUjemnyMnoznikBigDecimal {
 
 	@Test
 	public void test(){
 		
+		Money money1=new Money(4.0,"USD");
 		
 		
 	}
