19a893168087f3d0d4b623d5f92798fb8d25ced2
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:44:39 +0100

    Update testu MoneyGreaterThanNegatywnyWynik

--- src/test/java/lab2_2/MoneyGreaterThanNegatywnyWynik.java
+++ src/test/java/lab2_2/MoneyGreaterThanNegatywnyWynik.java
@@ -1,7 +1,17 @@
 package lab2_2;
 
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 public class MoneyGreaterThanNegatywnyWynik {
 	
-	
+	@Test
+	public void test(){
+		
+		
+		
+		
+	}
 
 }
