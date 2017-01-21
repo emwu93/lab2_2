1f3579e52c1f6cb3f09428b894997c354fd61bd4
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:14:39 +0100

    Zmiana nazwy testu oraz dodanie testu MoneyMultiplyByDodatniMnoznikDouble

---
+++ src/test/java/lab2_2/MoneyMultiplyByDodatniMnoznikBigDecimal.java
@@ -0,0 +1,23 @@
+package lab2_2;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import java.math.*;
+
+public class MoneyMultiplyByDodatniMnoznikBigDecimal {
+	
+	@Test
+	public void test(){
+		
+		Money money1=new Money(2.0,"USD");
+		Money oczekiwana_wart=new Money(4.0,"USD");
+		BigDecimal mnoznik= new BigDecimal(2.0);
+		
+		assertThat(oczekiwana_wart,is(equalTo(money1.multiplyBy(mnoznik))));
+		
+	}
+
+}
