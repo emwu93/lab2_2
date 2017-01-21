bef44497824845055d7e27ff8374f3b2dc57d1b9
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:37:43 +0100

    Update MoneyAddDodawanieRozneWaluty

---
+++ src/test/java/lab2_2/MoneyAddDodawanieRoznaWaluta.java
@@ -0,0 +1,24 @@
+package lab2_2;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class MoneyAddDodawanieRoznaWaluta {
+
+
+	@Test(expected=IllegalArgumentException.class)
+	public void test(){		
+		
+		Money money1=new Money(3.0,"USD");
+		Money money2=new Money(1.0, "EUR");
+		
+		
+	}
+	
+	
+}
