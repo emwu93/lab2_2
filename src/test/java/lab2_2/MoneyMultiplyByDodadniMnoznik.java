4be1fc06f4f5f01d51e4df834569f8abf3dd4098
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 16:09:34 +0100

    Update testu Money MultiplyByDodadniMnoznik v3

--- src/test/java/lab2_2/MoneyMultiplyByDodadniMnoznik.java
+++ src/test/java/lab2_2/MoneyMultiplyByDodadniMnoznik.java
@@ -13,9 +13,10 @@ public class MoneyMultiplyByDodadniMnoznik {
 	public void test(){
 		
 		Money money1=new Money(2.0,"USD");
+		Money oczekiwana_wart=new Money(4.0,"USD");
 		BigDecimal mnoznik= new BigDecimal(2.0);
 		
-		
+		assertThat(oczekiwana_wart,is(equalTo(money1.multiplyBy(mnoznik))));
 		
 	}
 
