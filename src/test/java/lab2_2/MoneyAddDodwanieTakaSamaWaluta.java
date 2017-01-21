1b6ae49507efb53bac5a6b3a268e99c6bc97b124
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-21 15:59:03 +0100

    update testu MoneySubstractTakaSamaWalutaWynikUjemny v2

--- src/test/java/lab2_2/MoneyAddDodwanieTakaSamaWaluta.java
+++ src/test/java/lab2_2/MoneyAddDodwanieTakaSamaWaluta.java
@@ -15,10 +15,7 @@ public class MoneyAddDodwanieTakaSamaWaluta {
 	Money money1=new Money(2.0,"USD");
 	Money money2=new Money(3.0,"USD");
 	Money oczekiwana_wart= new Money(5.0,"USD");
-	
-	Money suma=money1.add(money2);	
-	
-	assertThat(suma,is(equalTo(oczekiwana_wart)));
+	;
 	
 	
 	}
--- src/test/java/lab2_2/MoneySubstractTakaSamaWalutaWynikUjemny.java
+++ src/test/java/lab2_2/MoneySubstractTakaSamaWalutaWynikUjemny.java
@@ -1,6 +1,9 @@
 package lab2_2;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -11,6 +14,10 @@ public class MoneySubstractTakaSamaWalutaWynikUjemny {
 		
 		Money money1=new Money(2.0,"USD");
 		Money money2=new Money(3.0,"USD");
+		Money oczekiwana_wart = new Money(-1.0,"USD");
+		
+		assertThat(oczekiwana_wart,is(equalTo(money1.subtract(money2))));
+		
 		
 	}
 
